c6fa4fec9d645e0664089d42b7184b14f00fc0a2
JesusRamon2192 <jesus.ramon2192@gmail.com>
2024-04-19 11:21:58 -0600

    Clase 5 El objeto props y children

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -3,11 +3,9 @@ import type { FunctionComponent, FC } from "react";
 /* export const RandomFox = () => {
   return <img />;
 }; */
+type Props = {image: string}
 
-const random = () => Math.floor(Math.random() * 123) + 1;
-
-export const RandomFox = (): JSX.Element => {
-  const image: string = `https://randomfox.ca/images/${random()}.jpg`;
+export const RandomFox = ({image}: Props): JSX.Element => {
   return <img width={320} height="auto" src={image} className="rounded" />;
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { RandomFox } from "@/components/RandomFox";
 
 const inter = Inter({ subsets: ["latin"] });
+const random = () => Math.floor(Math.random() * 123) + 1;
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
 
       <main>
         <h1 className="text-3xl font-bold underline">Hey Platzi 😎!</h1>
-        <RandomFox />
+        <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`}/>
       </main>
 
       <footer></footer>
