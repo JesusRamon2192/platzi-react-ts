3bb54f5b094a87a64d8a80afe00aad611f22348d
Jesus Ramon <108687344+JesusRamon2192@users.noreply.github.com>
2024-04-20 22:43:05 +0000

    Clase 12 Sigamos extendiendo el DOM

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,42 +1,53 @@
-import { useRef, useEffect, useState, ImgHTMLAttributes } from "react";
+import { useRef, useEffect, useState } from "react";
+import type { ImgHTMLAttributes } from "react";
 
-type LazyImageProps = { src: string };
-type ImageNative = ImgHTMLAttributes<HTMLImageElement>;
-type Props = LazyImageProps & ImageNative;
+type LazyImageProps = {
+  src: string;
+  onLazyLoad?: (img: HTMLImageElement) => void;
+};
 
-export const LazyImage = ({ src, ...imgProps }: Props): JSX.Element => {
+type Props = ImgHTMLAttributes<HTMLImageElement> & LazyImageProps;
+
+export function LazyImage({
+  src,
+  onLazyLoad,
+  ...imgProps
+}: Props): JSX.Element {
   const node = useRef<HTMLImageElement>(null);
+  const [isLazyLoaded, setIsLazyLoaded] = useState(false);
   const [currentSrc, setCurrentSrc] = useState(
     "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=",
   );
 
   useEffect(() => {
+    if (isLazyLoaded) {
+      return;
+    }
+
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
-        //onIntersection -> consolelog
-        if (entry.isIntersecting) {
-          console.log("heyyou");
-          setCurrentSrc(src);
+        if (!entry.isIntersecting || !node.current) {
+          return;
+        }
+
+        setCurrentSrc(src);
+        observer.disconnect();
+        setIsLazyLoaded(true);
+
+        if (typeof onLazyLoad === "function") {
+          onLazyLoad(node.current);
         }
       });
     });
-    //observer node
+
     if (node.current) {
       observer.observe(node.current);
     }
 
-    //desconectar
     return () => {
       observer.disconnect();
     };
-  }, [src]);
+  }, [src, onLazyLoad, isLazyLoaded]);
 
-  return (
-    <img
-      ref={node}
-      src={currentSrc}
-      className="rounded bg-gray-300"
-      {...imgProps}
-    />
-  );
-};
+  return <img ref={node} src={currentSrc} {...imgProps} />;
+}
